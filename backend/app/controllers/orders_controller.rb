06914c47a440f69e2f84f25919927b27bfe0822f
Guilherme Linden <lindengui@gmail.com>
2017-12-14 23:20:01 +0100

    Added CRUD methods to order controller

--- backend/app/controllers/orders_controller.rb
+++ backend/app/controllers/orders_controller.rb
@@ -1,18 +1,32 @@
 class OrdersController < ApplicationController
+    
+    def index
+        orders = Order.order("created_at DESC")
+        render json: {status:"SUCCESS", message:"Loaded orders", data:orders}, status: :ok
+    end 
+
+    def show
+        order = Order.find(params[:id])
+        render json: {status:"SUCCESS",message:"Order successfully found", data:order},status: :ok
+    end
+    
     def create
         order = Order.new(order_params)
         if order.save
-            render json: {message:"Order successfully created", data:order},status: :ok
+            render json: {status:"SUCCESS",message:"Order successfully created", data:order},status: :ok
         else
-            render json: {message:"Cannot create order", data:order.erros},status: :unprocessable_entity
+            render json: {status:"ERROR",message:"Cannot create order", data:order.erros},status: :unprocessable_entity
         end
     end
 
-
     def update
-        
+        order = Order.find(params[:id])
+        if order.update_attributes(order_params)
+            render json: {status:"SUCCESS",message:"Order successfully updated", data:order},status: :ok
+        else
+            render json: {status:"ERROR",message:"Cannot update order", data:order.erros},status: :unprocessable_entity
+        end
     end
-
 
     def destroy
         order  = Order.find(params[:id])
