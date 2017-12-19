512025281e0fb0118deae0dc16ad60a4691573fa
Guilherme Linden <lindengui@gmail.com>
2017-12-19 17:14:40 +0100

    Removed event prevent default

--- frontend/js/routes.interceptor.js
+++ frontend/js/routes.interceptor.js
@@ -2,7 +2,6 @@
     angular
     .module('app')
     .run(function ($log,$location) {
-        event.preventDefault();
         $log.log("Run block test");        
         $location.path('/home');
         });    
