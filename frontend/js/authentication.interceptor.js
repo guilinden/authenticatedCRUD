1606c856234dce6dde71e82032a5f15f5a3818a0
Guilherme Linden <lindengui@gmail.com>
2017-12-19 16:57:04 +0100

    Run block error found

--- frontend/js/authentication.interceptor.js
+++ frontend/js/authentication.interceptor.js
@@ -1,22 +1,9 @@
 (function(){
     angular
     .module('app')
-    .run(function ($rootScope) {
-        
-          $rootScope.$on('$stateChangeStart','$log','$location', function (event, toState, toParams) {
-            var requireLogin = toState.data.requireLogin;
-        
-            if (requireLogin) {
-              event.preventDefault();
-              $location.path('/');
-              $log.log('Teste');
-            }
-            else{
-                $log.log('Teste 2');
-                $location.path('/');
-            }
-          });
-        
+    .run(function () {
+        event.preventDefault();
+        $log.log("Run block test");        
         });
         
 });
